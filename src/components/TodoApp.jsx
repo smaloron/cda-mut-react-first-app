199fae86aa88a8df0fb2de5bc200619810e3c114
seb <contact@smaloron.eu>
2026-02-16 16:18:01 +0100

    Add list filtering

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,6 +1,12 @@
 import {useState} from "react";
 import TodoList from "./TodoList.jsx";
 
+// Constante pour les états du filtre
+const FILTERS = {
+    ALL: 'all',
+    DONE: 'done',
+    PENDING: 'pending',
+}
 
 function TodoApp(){
     const [todos, setTodos] = useState([
@@ -8,6 +14,13 @@ function TodoApp(){
         {id: 2, title: 'Tuer le lion de Némée', done: true },
         {id: 3, title: 'Délivrer Thésée des enfers', done: false },
     ]);
+    const [filter, setFilter] = useState(FILTERS.ALL);
+
+    const filterFunctions = {
+        [FILTERS.ALL]: (todos) => todos,
+        [FILTERS.DONE]: (todos) => todos.filter((item) => item.done),
+        [FILTERS.PENDING]: (todos) => todos.filter((item) => !item.done),
+    }
 
     const [inputTitle, setInputTitle] = useState("");
 
@@ -40,7 +53,8 @@ function TodoApp(){
         }
     }
 
-    console.log(todos);
+    // Exécution du filtre
+    const filteredTodos = (filterFunctions[filter] || filterFunctions[FILTERS.ALL])(todos);
 
     return (
         <>
@@ -51,7 +65,13 @@ function TodoApp(){
                        placeholder="Ajouter une tâche"/>
                 <button type="submit">Ajouter</button>
             </form>
-            <TodoList todos={todos} toggleDone={toggleDone} deleteTodo={deleteTodo} />
+
+            <div>
+                <button onClick={()=>setFilter(FILTERS.ALL)}>Toutes</button>
+                <button onClick={()=>setFilter(FILTERS.DONE)}>Terminées</button>
+                <button onClick={()=>setFilter(FILTERS.PENDING)}>En cours</button>
+            </div>
+            <TodoList todos={filteredTodos} toggleDone={toggleDone} deleteTodo={deleteTodo} />
         </>
     );
 }
